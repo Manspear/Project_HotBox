5474ed846a933a7a343e38e7684df4632c914d9d
pixet93 <erik_pettersson@outlook.com>
2016-10-14 21:37:59 +0200

    EN: Removed hardcoded cube.

--- GamePlay/SceneViewer/src/HSceneViewer.cpp
+++ GamePlay/SceneViewer/src/HSceneViewer.cpp
@@ -42,87 +42,6 @@ void HSceneViewer::initialize()
 	_scene->addNode(lightNode);
 	lightNode->release();
 	light->release();
-
-	/*float size = 1.0f;
-
-	float a = size * 0.5f;
-
-	float vertices[] =
-	{
-		-a, -a,  a,    0.0,  0.0,  1.0,   0.0, 0.0,
-		a, -a,  a,    0.0,  0.0,  1.0,   1.0, 0.0,
-		-a,  a,  a,    0.0,  0.0,  1.0,   0.0, 1.0,
-		a,  a,  a,    0.0,  0.0,  1.0,   1.0, 1.0,
-		-a,  a,  a,    0.0,  1.0,  0.0,   0.0, 0.0,
-		a,  a,  a,    0.0,  1.0,  0.0,   1.0, 0.0,
-		-a,  a, -a,    0.0,  1.0,  0.0,   0.0, 1.0,
-		a,  a, -a,    0.0,  1.0,  0.0,   1.0, 1.0,
-		-a,  a, -a,    0.0,  0.0, -1.0,   0.0, 0.0,
-		a,  a, -a,    0.0,  0.0, -1.0,   1.0, 0.0,
-		-a, -a, -a,    0.0,  0.0, -1.0,   0.0, 1.0,
-		a, -a, -a,    0.0,  0.0, -1.0,   1.0, 1.0,
-		-a, -a, -a,    0.0, -1.0,  0.0,   0.0, 0.0,
-		a, -a, -a,    0.0, -1.0,  0.0,   1.0, 0.0,
-		-a, -a,  a,    0.0, -1.0,  0.0,   0.0, 1.0,
-		a, -a,  a,    0.0, -1.0,  0.0,   1.0, 1.0,
-		a, -a,  a,    1.0,  0.0,  0.0,   0.0, 0.0,
-		a, -a, -a,    1.0,  0.0,  0.0,   1.0, 0.0,
-		a,  a,  a,    1.0,  0.0,  0.0,   0.0, 1.0,
-		a,  a, -a,    1.0,  0.0,  0.0,   1.0, 1.0,
-		-a, -a, -a,   -1.0,  0.0,  0.0,   0.0, 0.0,
-		-a, -a,  a,   -1.0,  0.0,  0.0,   1.0, 0.0,
-		-a,  a, -a,   -1.0,  0.0,  0.0,   0.0, 1.0,
-		-a,  a,  a,   -1.0,  0.0,  0.0,   1.0, 1.0
-	};
-
-	short indices[] =
-	{
-		0, 1, 2, 2, 1, 3, 4, 5, 6, 6, 5, 7, 8, 9, 10, 10, 9, 11, 12, 13, 14, 14, 13, 15, 16, 17, 18, 18, 17, 19, 20, 21, 22, 22, 21, 23
-	};
-
-	unsigned int vertexCount = 24;
-	unsigned int indexCount = 36;
-
-	VertexFormat::Element elements[] =
-	{
-		VertexFormat::Element(VertexFormat::POSITION, 3),
-		VertexFormat::Element(VertexFormat::NORMAL, 3),
-		VertexFormat::Element(VertexFormat::TEXCOORD0, 2)
-	};
-
-	Mesh* mesh = Mesh::createMesh(VertexFormat(elements, 3), vertexCount, false);
-
-	mesh->setVertexData(vertices, 0, vertexCount);
-
-	MeshPart* meshPart = mesh->addPart(Mesh::TRIANGLES, Mesh::INDEX16, indexCount, false);
-
-	meshPart->setIndexData(indices, 0, indexCount);
-
-	Model* cubeModel = Model::create(mesh);
-
-	mesh->release();
-
-	Material* material = cubeModel->setMaterial("res/shaders/colored.vert", "res/shaders/colored.frag", "DIRECTIONAL_LIGHT_COUNT 1");
-
-	material->setParameterAutoBinding("u_worldViewProjectionMatrix", "WORLD_VIEW_PROJECTION_MATRIX");
-	material->setParameterAutoBinding("u_inverseTransposeWorldViewMatrix", "INVERSE_TRANSPOSE_WORLD_VIEW_MATRIX");
-
-	material->getParameter("u_ambientColor")->setValue(Vector3(0.2f, 0.2f, 0.2f));
-
-	material->getParameter("u_directionalLightColor[0]")->setValue(lightNode->getLight()->getColor());
-	material->getParameter("u_directionalLightDirection[0]")->bindValue(lightNode, &Node::getForwardVectorWorld);
-
-	Node* meshNode = Node::create("cube");
-	
-	Node* asdf = Node::create("shit");
-
-	meshNode->translate(0.f, 0.f, -7.f);
-
-	meshNode->setDrawable(cubeModel);
-
-	_scene->addNode(meshNode);
-
-	cubeModel->release();*/
 }
 
 void HSceneViewer::finalize()
@@ -142,18 +61,8 @@ what happened to that node --> have some bools
 void HSceneViewer::update(float elapsedTime)
 {
 	/*Get the information we send from the Maya plugin here.*/
-	//HMessageReader::MessageType messageType;
 	HMessageReader::sFoundInfo nfo;
 
-	/*
-	fRead calls functions that...
-	Why don't we just create the nodes directly
-	from the data read from the message?
-	like:
-	"read main header"
-	"see what kind of item is in the message"
-	"make that node in the 
-	*/
 	msgReader->fRead(msgReader->circBuff, _scene);
 }
 
