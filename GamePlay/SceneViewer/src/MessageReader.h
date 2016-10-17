beb94c7db7ad78e66d24ae7562af4945cc9cc4c3
Manspear <albincarlsson13@gmail.com>
2016-10-17 11:25:39 +0200

    Some comments on functions

--- GamePlay/SceneViewer/src/MessageReader.h
+++ GamePlay/SceneViewer/src/MessageReader.h
@@ -59,7 +59,9 @@ public:
 	/*Functions for processing deleted nodes*/
 	void fProcessDeletedObject(char* messageData, gameplay::Scene* scene);
 
-	/*Functions for processing mesh messages, getting the newly mesh data and also the updated vertices from the mesh.*/
+	/*
+	Functions for processing mesh messages, getting the newly mesh data and also the updated vertices from the mesh.
+	*/
 	void fProcessMesh(char* messageData, gameplay::Scene* scene);
 
 	/*Functions for processing material messages, getting the newly material data abd update a existing mesh material.*/
@@ -68,7 +70,12 @@ public:
 	/*Functions for processing light messages and getting the new light.*/
 	void fProcessLight(char* messageData, gameplay::Scene* scene);
 
-	/*Functions for processing transform messages and getting the new transform.*/
+	/*
+	Functions for processing transform messages and getting the new transform.
+	Creates a node with the same name as it's child node if it doesn't exist.
+	Depending on it's child node type, it will create the node differently
+	(if necessary)
+	*/
 	void fProcessTransform(char* messageData, gameplay::Scene* scene);
 
 	/*Functions for processing camera messages and getting the new camera.*/
@@ -78,8 +85,15 @@ public:
 	sFoundInfo fFindMesh(const char* mName);
 	
 private:
-
-	
+	/*Functions for creating and modifying meshes*/
+	void fCreateNewMeshNode(char* meshName, hMeshVertex& vertList, 
+						    hMeshHeader& meshHeader, gameplay::Node* nd,
+							gameplay::Scene* scene);
+	/*
+	Applies its transform to the transform's node, and sets a child if there is one
+	We need to be able to have more than one child in the future.
+	*/
+	void fModifyNodeTransform(hTransformHeader& transH, gameplay::Node* nd, gameplay::Scene* scene);
 };
 
 
